5630d6e4f6eec100f312cd55f249e7e17232917d
Maria <mariaed.cassiano@gmail.com>
2023-01-29 19:48:58 -0300

    Criada controller de usuario e models de usuario e tarefa

---
+++ SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SistemaDeTarefas/Models/TarefaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int Status { get; set; }
+    }
+}
